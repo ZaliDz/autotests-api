6803cef61ffce92500730de23b1f6913753febe3
ZaliDz <132756234+ZaliDz@users.noreply.github.com>
2025-06-04 16:49:00 +0300

    Знакомство с API клиентами. Практика: Написание API клиента

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,29 @@
+from api_client import APIClient
+from httpx import Response
+from typing import TypedDict
+
+
+class CreateRequestDict(TypedDict):
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+class PublicUsersClient(APIClient):
+
+    '''
+    Клиент для работы с /api/v1/users
+    '''
+
+    def create_user_api(self, request: CreateRequestDict) -> Response:
+        '''
+        Метод выполняет регистрацию пользователя.
+
+        :param request: Словарь с email, password, lastName, firstName, middleName
+        :return: Ответ от сервера в виде объекта httpx.Response
+        '''
+
+        self.post("api/v1/users", json=request)
+
