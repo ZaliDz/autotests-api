f7711675c647a2d8b3f9446ad0be999eadeed3b3
ZaliDz <132756234+ZaliDz@users.noreply.github.com>
2025-06-05 17:33:39 +0300

    Применяем Pydantic в API клиентах. Практикуемся в применении Pydantic в API клиентах

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,13 +1,14 @@
-from clients.exercises.exercises_client import get_exercises_client, CreateExercisesRequestDict
-from clients.private_http_builder import AuthenticationUserDict
-from clients.files.files_client import get_files_client, CreateFileRequestDict
-from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.courses.courses_client import get_courses_client, CreateCourseRequestSchema
+from clients.exercises.exercises_client import get_exercises_client
+from clients.files.files_client import get_files_client, CreateFileRequestSchema
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.public_users_client import get_public_users_client, CreateUserRequestSchema
 from tools.fakers import get_random_email
+from clients.exercises.exercises_schema import CreateExerciseRequestSchema
 
 public_users_client = get_public_users_client()
 
-create_user_request = CreateUserRequestDict(
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="string",
     lastName="string",
@@ -16,12 +17,15 @@ create_user_request = CreateUserRequestDict(
 )
 create_user_response = public_users_client.create_user(create_user_request)
 
-authentication_user = AuthenticationUserDict(
-    email=create_user_request['email'],
-    password=create_user_request['password']
+authentication_user = AuthenticationUserSchema(
+    email=create_user_request.email,
+    password=create_user_request.password
 )
 files_client = get_files_client(authentication_user)
-create_file_request = CreateFileRequestDict(
+courses_client = get_courses_client(authentication_user)
+exercises_client = get_exercises_client(authentication_user)
+
+create_file_request = CreateFileRequestSchema(
     filename="image.png",
     directory="courses",
     upload_file="./testdata/files/image.png"
@@ -29,29 +33,26 @@ create_file_request = CreateFileRequestDict(
 create_file_response = files_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
-courses_client = get_courses_client(authentication_user)
-create_course_request = CreateCourseRequestDict(
+create_course_request = CreateCourseRequestSchema(
     title="Python",
     maxScore=100,
     minScore=10,
     description="Python API course",
     estimatedTime="2 weeks",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    previewFileId=create_file_response.file.id,
+    createdByUserId=create_user_response.user.id
 )
 create_course_response = courses_client.create_course(create_course_request)
 print('Create course data:', create_course_response)
 
-exercises_client = get_exercises_client(authentication_user)
-
-create_exercises_request = CreateExercisesRequestDict(
-    title="Python",
-    courseId=create_course_response['course']['id'],
-    maxScore=100,
-    minScore=10,
-    description="Python API exercise",
-    estimatedTime="2 weeks",
-    orderIndex=5
+create_exercise_request = CreateExerciseRequestSchema(
+    title="Exercise 1",
+    courseId=create_course_response.course.id,
+    maxScore=5,
+    minScore=1,
+    orderIndex=0,
+    description="Exercise 1",
+    estimatedTime="5 minutes"
 )
-create_exercises_response = exercises_client.create_exercise(create_exercises_request)
-print('Create exercise data:', create_exercises_response)
+create_exercise_response = exercises_client.create_exercise(create_exercise_request)
+print('Create exercise data:', create_exercise_response)
