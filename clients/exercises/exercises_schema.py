4c772e07de63cea4207f30b806c5f0a1b0edcf17
ZaliDz <132756234+ZaliDz@users.noreply.github.com>
2025-06-06 16:22:44 +0300

    Знакомство с Faker. Практика работы с генерацией случайных данных

--- clients/exercises/exercises_schema.py
+++ clients/exercises/exercises_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field, ConfigDict
+from tools.fakers import fake
 
 
 class ExerciseSchema(BaseModel):
@@ -33,25 +34,25 @@ class UpdateExerciseRequestSchema(BaseModel):
     '''
     Описание структуры запроса обновления упражнения
     '''
-    title: str = Field(default=None)
-    course_id: str = Field(default=None, alias="courseId")
-    max_score: int = Field(default=None, alias="maxScore")
-    min_score: int = Field(default=None, alias="minScore")
-    order_index: int = Field(default=None, alias="orderIndex")
-    description: str = Field(default=None)
-    estimated_time: str = Field(default=None, alias="estimatedTime")
+    title: str | None = Field(default_factory=fake.sentence)
+    course_id: str | None = Field(alias="courseId", default_factory=fake.uuid4)
+    max_score: int | None = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
+    order_index: int | None = Field(alias="orderIndex", default_factory=fake.integer)
+    description: str | None = Field(default_factory=fake.text)
+    estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 class CreateExerciseRequestSchema(BaseModel):
     '''
     Описание структуры запроса обновления упражнения
     '''
-    title: str
-    course_id: str = Field(alias="courseId")
-    max_score: int = Field(alias="maxScore")
-    min_score: int = Field(alias="minScore")
-    order_index: int = Field(alias="orderIndex")
-    description: str
-    estimated_time: str = Field(alias="estimatedTime")
+    title: str = Field(default_factory=fake.sentence)
+    course_id: str = Field(alias="courseId", default_factory=fake.uuid4)
+    max_score: int = Field(alias="maxScore", default_factory=fake.max_score)
+    min_score: int = Field(alias="minScore", default_factory=fake.min_score)
+    order_index: int = Field(alias="orderIndex", default_factory=fake.integer)
+    description: str = Field(default_factory=fake.text)
+    estimated_time: str = Field(alias="estimatedTime", default_factory=fake.estimated_time)
 
 class CreateExerciseResponseSchema(BaseModel):
     '''
@@ -70,4 +71,3 @@ class GetExercisesResponseSchema(BaseModel):
     Описание структуры ответа на получение списка заданий.
     """
     exercise: list[ExerciseSchema]
-
