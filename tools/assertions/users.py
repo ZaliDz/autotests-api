bac494e45f60c380ee3b9d427b7b8bf311a2deda
ZaliDz <132756234+ZaliDz@users.noreply.github.com>
2025-06-07 16:29:00 +0300

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

---
+++ tools/assertions/users.py
@@ -0,0 +1,41 @@
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from tools.assertions.base import assert_equal
+from clients.users.users_schema import UserSchema
+
+
+def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
+    """
+    Проверяет, что ответ на создание пользователя соответствует запросу.
+
+    :param request: Исходный запрос на создание пользователя.
+    :param response: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(response.user.email, request.email, "email")
+    assert_equal(response.user.last_name, request.last_name, "last_name")
+    assert_equal(response.user.first_name, request.first_name, "first_name")
+    assert_equal(response.user.middle_name, request.middle_name, "middle_name")
+
+def assert_user(actual: UserSchema, expected: UserSchema):
+    """
+    Проверяет корректность данных пользователя.
+
+    :param actual: Исходный запрос.
+    :param expected: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.user.id, expected.user.id, "id")
+    assert_equal(actual.user.email, expected.user.email, "email")
+    assert_equal(actual.user.last_name, expected.user.last_name, "last_name")
+    assert_equal(actual.user.first_name, expected.user.first_name, "first_name")
+    assert_equal(actual.user.middle_name, expected.user.middle_name, "middle_name")
+
+def assert_get_user_response(get_user_response, create_user_response):
+    """
+    Проверяет, что данные пользователя при создании и при запросе совпадают.
+
+    :param get_user_response: Ответ API при создании пользователя.
+    :param create_user_response: Ответ API при создании пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_user(get_user_response, create_user_response)
