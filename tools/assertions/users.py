507c774940e0957c89fa124c79f33e748eafebba
ZaliDz <132756234+ZaliDz@users.noreply.github.com>
2025-06-14 11:50:31 +0300

    Запуск автотестов на CI. Практика работы с GitHub Actions

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,8 +1,13 @@
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_equal
 from clients.users.users_schema import UserSchema
+import allure
+from tools.logger import get_logger  # Импортируем функцию для создания логгера
+
+logger = get_logger("USERS_ASSERTIONS") 
 
 
+@allure.step("Check create user response")
 def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
     """
     Проверяет, что ответ на создание пользователя соответствует запросу.
@@ -11,25 +16,33 @@ def assert_create_user_response(request: CreateUserRequestSchema, response: Crea
     :param response: Ответ API с данными пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
+    logger.info("Check create user response")
+
     assert_equal(response.user.email, request.email, "email")
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
     assert_equal(response.user.middle_name, request.middle_name, "middle_name")
 
+@allure.step("Check user")
 def assert_user(actual: UserSchema, expected: UserSchema):
     """
-    Проверяет корректность данных пользователя.
+    Проверяет, что фактические данные пользователя соответствуют ожидаемым.
 
-    :param actual: Исходный запрос.
-    :param expected: Ответ API с данными пользователя.
+    :param actual: Фактические данные пользователя.
+    :param expected: Ожидаемые данные пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_equal(actual.user.id, expected.user.id, "id")
-    assert_equal(actual.user.email, expected.user.email, "email")
-    assert_equal(actual.user.last_name, expected.user.last_name, "last_name")
-    assert_equal(actual.user.first_name, expected.user.first_name, "first_name")
-    assert_equal(actual.user.middle_name, expected.user.middle_name, "middle_name")
 
+    logger.info("Check user")
+
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.email, expected.email, "email")
+    assert_equal(actual.last_name, expected.last_name, "last_name")
+    assert_equal(actual.first_name, expected.first_name, "first_name")
+    assert_equal(actual.middle_name, expected.middle_name, "middle_name")
+
+
+@allure.step("Check get user response")
 def assert_get_user_response(get_user_response, create_user_response):
     """
     Проверяет, что данные пользователя при создании и при запросе совпадают.
@@ -38,4 +51,7 @@ def assert_get_user_response(get_user_response, create_user_response):
     :param create_user_response: Ответ API при создании пользователя.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_user(get_user_response, create_user_response)
+
+    logger.info("Check get user response")
+
+    assert_user(get_user_response.user, create_user_response.user)
